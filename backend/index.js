66112172b5bce0a06182d01a7001e1111955c7de
Animesh Raj <animesh080808@gmail.com>
2023-05-13 05:03:18 +0530

    fixed
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const https = require('https');
-const http = require('http');
-const app = express();
-const fs = require('fs');
 
+const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
@@ -12,15 +9,6 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('/names', (req, res) => {
     res.send({ message: 'Start of  things' });
 });
-
-const options = {
-    key: fs.readFileSync(path.join(__dirname, './ssl/private.key')),
-    cert: fs.readFileSync(path.join(__dirname, './ssl/certificate.crt')),
-    ca: fs.readFileSync(path.join(__dirname, './ssl/ca_bundle.crt')),
-};
-
-const PORT = 8000;
-const server = https.createServer(options, app);
-server.listen(PORT, function () {
-    console.log(`server at port ${PORT}`);
+app.listen(8000, () => {
+    console.log('Running on port 8000');
 });
