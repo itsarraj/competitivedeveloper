8961ef50ce74ce8df78c58709eb09c30e74d72ab
Animesh Raj <animesh080808@gmail.com>
2023-05-13 00:59:55 +0530

    Configured Github Actions
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('/home', (req, res) => {
+    res.send('Start of great things');
+});
+
+app.listen(8000, () => {
+    console.log('Running on port 8000');
+});
