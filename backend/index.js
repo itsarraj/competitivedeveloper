bc00e6cd84a82eb53ef6afb0720a329552d47bc9
Animesh Raj <animesh080808@gmail.com>
2023-05-14 23:31:52 +0530

    started working in routes

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,11 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
-let allowed = [
-    'http://localhost:4000',
-    'http://localhost:3001', // multiple ports allowed
-    'http://localhost:3002', // multiple ports allowed
-];
-function options(req, res) {
-    let tmp;
-    let origin = req.header('Origin');
-    if (allowed.indexOf(origin) > -1) {
-        tmp = {
-            origin: true,
-            optionSuccessStatus: 200,
-        };
-    } else {
-        tmp = {
-            origin: false,
-        };
-    }
-    res(null, tmp);
-}
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
-// const options = {
-//     origin: 'http://localhost:3000',
-//     useSuccessStatus: 200,
-// };
-app.use(cors(options));
+const { readdirSync } = require('fs');
+app.use(cors());
 
 const path = require('path');
 
@@ -35,6 +14,16 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('/', (req, res) => {
     res.send({ message: 'Start of  things' });
 });
-app.listen(8000, () => {
-    console.log('Running on port 8000');
+
+//  We can use this or can create index.js inside /routes
+readdirSync('./routes').map((r) => {
+    app.use('/', require('./routes/' + r));
+});
+
+// app.use('/', require('./routes/index'));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
 });
