d1831b47cbb54ade1d87fe1ccc92958f74d0c1c0
Animesh Raj <animesh080808@gmail.com>
2023-05-14 07:34:08 +0530

    Modified few texts
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,10 @@ function App() {
     return (
         <div className="App">
             <h1>Finally Everything is Setup</h1>
+            <h1>
+                Project Started __ You will see a fun website for developers
+                soon
+            </h1>
             <h1>frontend {aaaa} </h1> <h1>frontend {aaaa} </h1>{' '}
             <h1>frontend {aaaa} </h1>{' '}
         </div>
