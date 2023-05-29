cf73a5d3807e64d46980e93e4d1a47c981142efe
Animesh Raj <animesh080808@gmail.com>
2023-05-29 09:32:09 +0530

    feat : files
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers/index.js';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import rootReducer from './reducers/index.js';
 
-const store = createStore(rootReducer, composeWithDevTools());
+// const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
+        {/* <Provider store={store}> */}
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+        {/* </Provider> */}
     </React.StrictMode>
 );
--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,36 +1,73 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import styles from './login.module.css';
-import Link from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-export default function Login() {
+function Login() {
     return (
-        <div className="login">
-            <div className="login_wrapper">
-                <div className="login_wrap">
-                    <div className="login_1">
-                        <img
-                            src="https://akm-img-a-in.tosshub.com/businesstoday/images/story/202303/twitter-feature-sixteen_nine.jpg?size=948:533"
-                            alt="Logo"
-                        />
-                        <span>We help developer connect with each other </span>
-                    </div>
-                    <div className="login_2">
-                        <div className="login_2_wrap">
-                            <Formik>
-                                {(formik) => (
-                                    <Form>
-                                        <input type="text" />
-                                        <input type="text" />
-                                        <button type="submit">Login</button>
-                                    </Form>
-                                )}
-                            </Formik>
-                            <Link to="/forgot">Forgtten Password ?</Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="register"></div>
+        <div class="container">
+        <form id="form" class="form">
+            <h2>Registration Form</h2>
+            <div class="form-control">
+                <label for="username">Username</label>
+                <input type="text" id="username" placeholder="Enter username">
+                <small>Error message</small>
             </div>
-        </div>
+            <div class="form-control">
+                <label for="email">Email</label>
+                <input type="text" id="email" placeholder="Enter email">
+                <small>Error message</small>
+            </div>
+            <div class="form-control">
+                <label for="password">Password</label>
+                <input type="password" id="password" placeholder="Enter password">
+                <small>Error message</small>
+            </div>
+            <div class="form-control">
+                <label for="password">Confirm Password</label>
+                <input type="password" id="password2" placeholder="Enter password">
+                <small>Error message</small>
+            </div>
+            <button>Submit</button>
+        </form>
+    </div>
+
+        // <div className="login">
+        //     <div className="login_wrapper">
+        //         <div className="login_wrap">
+        //             <div className="login_1">
+        //                 <img
+        //                     src="https://akm-img-a-in.tosshub.com/businesstoday/images/story/202303/twitter-feature-sixteen_nine.jpg?size=948:533"
+        //                     alt="Logo"
+        //                 />
+        //                 <span>We help developer connect with each other </span>
+        //             </div>
+        //             <div className="login_2">
+        //                 <div className="login_2_wrap">
+        //                     <Formik>
+        //                         {(formik) => (
+        //                             <Form>
+        //                                 <input type="text" />
+        //                                 <input type="text" />
+        //                                 <button type="submit">Login</button>
+        //                             </Form>
+        //                         )}
+        //                     </Formik>
+        //                     <Link to="/forgot">Forgtten Password ?</Link>
+        //                     <div className="sign_splitter"></div>
+        //                     <button className="blue_btn open_signup">
+        //                         Create Account
+        //                     </button>
+        //                 </div>
+        //                 <Link to="/">
+        //                     <b>Create a Page</b>
+        //                     for a celebrity, band or business.
+        //                 </Link>
+        //             </div>
+        //         </div>
+        //         <div className="register"></div>
+        //     </div>
+        // </div>
     );
 }
+
+export default Login;
