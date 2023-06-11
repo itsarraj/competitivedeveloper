daf39d59225e2b3805485e26f11ac0082d5c8b86
Animesh Raj <animesh080808@gmail.com>
2023-06-11 19:11:52 +0530

    feat : git add -A

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-import styles from './module.app.css';
 import { Home, Login, Notfound, Register } from './pages/index.js';
+import Navbar from './components/Navbar/Navbar.jsx';
 function App() {
     return (
-        <div className={styles.body}>
+        <div>
+            <Navbar />
+
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
--- frontend/src/pages/notfound/notfound.jsx
+++ frontend/src/pages/notfound/notfound.jsx
@@ -6,13 +6,6 @@ function Notfound() {
         <div className={styles.body}>
             <div className={styles.container}>
                 <div className={styles.content}>
-                    <div className={styles.ellipserighttop}>
-                        <img
-                            src="/assets/notfound/ellipserighttop.png"
-                            alt="ellipserighttop"
-                        />
-                    </div>
-
                     <div className={styles.notfoundimage}>
                         <div className={styles.notfoundtxt}>
                             <span className={styles.oops}>Opss...</span>
@@ -22,13 +15,6 @@ function Notfound() {
                         </div>
 
                         <img src="/assets/notfound/notfound404.png" alt="404" />
-                    </div>
-
-                    <div className={styles.ellipseleftbottom}>
-                        <img
-                            src="/assets/notfound/ellipseleftbottom.png"
-                            alt="ellipseleftbottom"
-                        />
                     </div>
                 </div>
             </div>
