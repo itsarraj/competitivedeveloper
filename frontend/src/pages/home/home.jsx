cf7566fb3ced7ca53ea3522b49376ba09105f87f
Animesh Raj <animesh080808@gmail.com>
2023-06-20 21:54:21 +0530

    feat : adding files
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,52 +1,52 @@
-import styles from './home.module.scss';
-import { userSelector } from '../../reducers/userReducer';
-import CreatePost from '../../components/Home/CreatePost';
-import { useDispatch, useSelector } from 'react-redux';
-import { postActions, postSelector } from '../../reducers/postReducer';
-import PostSection from '../../components/Home/PostSection';
-import { useEffect } from 'react';
-import axios from 'axios';
+import styles from "./home.module.scss";
+import { userSelector } from "../../reducers/userReducer";
+import CreatePost from "../../components/Home/CreatePost";
+import { useDispatch, useSelector } from "react-redux";
+import { postActions, postSelector } from "../../reducers/postReducer";
+import PostSection from "../../components/Home/PostSection";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
-    const user = useSelector(userSelector);
-    const posts = useSelector(postSelector);
-    console.log('selector ', posts);
+  const user = useSelector(userSelector);
+  const posts = useSelector(postSelector);
+  console.log("selector ", posts);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const getAllPosts = async () => {
-            try {
-                const { data } = await axios.get(
-                    `${import.meta.env.VITE_BACKEND_URL}/get-all-posts`,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${user.token}`,
-                        },
-                    }
-                );
-                dispatch(postActions.setPost(data));
-            } catch (error) {
-                console.log(error);
-            }
-        };
+  useEffect(() => {
+    const getAllPosts = async () => {
+      try {
+        const { data } = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/get-all-posts`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.token}`,
+            },
+          }
+        );
+        dispatch(postActions.setPost(data));
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-        if (user && user.token && !posts.length) {
-            console.log('getAllPosts called');
-            getAllPosts();
-        }
-    }, []); // Add 'posts' as a dependency
+    if (user && user.token && !posts.length) {
+      console.log("getAllPosts called");
+      getAllPosts();
+    }
+  }, [posts]); // Add 'posts' as a dependency
 
-    // Empty dependency array to trigger the effect only once
+  // Empty dependency array to trigger the effect only once
 
-    return (
-        <div className={styles.body}>
-            <div className={styles.container}>
-                <CreatePost user={user} />
-                {posts.map((post) => (
-                    <PostSection key={post._id} post={post} />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.body}>
+      <div className={styles.container}>
+        <CreatePost user={user} />
+        {posts.map((post) => (
+          <PostSection key={post._id} post={post} />
+        ))}
+      </div>
+    </div>
+  );
 }
